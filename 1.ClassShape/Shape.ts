3ae7859b0af0b295dccc7b79162ef986bb930ec0
HieuT23511 <hieut23511@gmail.com>
2023-04-07 05:58:34 +0700

    1.ClassShape

---
+++ 1.ClassShape/Shape.ts
@@ -0,0 +1,30 @@
+export class Shape {
+    private _color: string;
+    private _filled: boolean;
+
+    constructor(color: string, filled: boolean) {
+        this._color = color;
+        this._filled = filled;
+    }
+
+    getColor(): string {
+        return this._color;
+    }
+
+    isFilled(): boolean {
+        return this._filled;
+    }
+
+    setColor(inputColor: string) {
+        this._color = inputColor;
+    }
+
+    setFilled(inputBoolean: boolean) {
+        this._filled = inputBoolean;
+    }
+
+    toString() {
+        return `A Shape with color of ${this.getColor()} and
+        ${this._filled ? "filled" : "not filled"}`
+    }
+}
